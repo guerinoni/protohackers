0556dd8b0108ec2442e79ee762d0094db2d92adf
Federico Guerinoni <guerinoni.federico@gmail.com>
2024-07-06 15:09:07 +0200

    Support shell.nix for linux and macos

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@
 pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     cargo
+  ] ++ pkgs.lib.optionals pkg.stdenv.isDarwin [
     darwin.apple_sdk.frameworks.SystemConfiguration
     libiconv
   ];
